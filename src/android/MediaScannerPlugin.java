bbc4d3ce0832a1c7b6e958d13db58f7e71148228
Peter Gao <pgao@quantcast.com>
2013-09-24 04:02:52 -0700

    Add JSONObject to the import statements

--- src/android/MediaScannerPlugin.java
+++ src/android/MediaScannerPlugin.java
@@ -5,6 +5,7 @@ import org.apache.cordova.CordovaPlugin;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Intent;
 import android.net.Uri;
